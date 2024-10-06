5dd79cfceca36d1670a045f64bd5bef7c1ebb68a
Dimitri <DimitriBolt@Arizona.edu>
2024-10-06 15:06:15 -0700

    add py

--- gauss_solve.py
+++ gauss_solve.py
@@ -10,6 +10,7 @@
 # A Python wrapper module around the C library libgauss.so
 
 import ctypes
+import numpy as np
 
 gauss_library_path = './libgauss.so'
 
@@ -53,6 +54,40 @@ def lu_c(A):
     # Extract L and U parts from A, fill with 0's and 1's
     return unpack(modified_array_2d)
 
+def plu_c(A):
+    """ Accepts a list of lists A of floats and
+    it returns (L, U) - the LU-decomposition as a tuple.
+    """
+    # Load the shared library
+    lib = ctypes.CDLL(gauss_library_path)
+
+    # Create a 2D array in Python and flatten it
+    n = len(A)
+    flat_array_2d = [item for row in A for item in row]
+
+    # Convert to a ctypes array
+    c_array_2d = (ctypes.c_double * len(flat_array_2d))(*flat_array_2d)
+
+    # Define the function signature
+    lib.lu_in_place.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_double))
+
+    # Modify the array in C (e.g., add 10 to each element)
+    lib.lu_in_place(n, c_array_2d)
+
+    # Convert back to a 2D Python list of lists
+    modified_array_2d = [
+        [c_array_2d[i * n + j] for j in range(n)]
+        for i in range(n)
+    ]
+
+    p_matrix = [[1, 0, 0],
+                [0, 1, 0],
+                [0, 1, 0]]
+     
+    # Extract L and U parts from A, fill with 0's and 1's
+    return p_matrix, unpack(modified_array_2d)
+
+
 def lu_python(A):
     n = len(A)
     for k in range(n):
@@ -73,6 +108,11 @@ def lu(A, use_c=False):
     else:
         return lu_python(A)
 
+def plu(A, use_c=False):
+    if use_c:
+        return plu_c(A)
+    else:
+        return lu_python(A)
 
 
 if __name__ == "__main__":
@@ -96,3 +136,6 @@ if __name__ == "__main__":
     L, U = lu(A, use_c=True)
     print(L)
     print(U)
+
+    use_c = True;
+    P, L, U = plu(A, use_c = use_c)
